a53a0dac7d57231df6eb5c71e41f556055e01b26
ovERthinkER7 <darshandd098@gmail.com>
2023-11-20 12:10:46 +0530

    fixed todos and firebase

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,225 +1,231 @@
 import React, { useEffect, useState, useContext } from "react";
 import { db } from "../../firebase";
 import {
-  getDocs,
-  collection,
-  addDoc,
-  deleteDoc,
-  doc,
-  query,
-  where,
-  updateDoc,
+    getDocs,
+    collection,
+    addDoc,
+    deleteDoc,
+    doc,
+    query,
+    where,
+    updateDoc,
 } from "firebase/firestore";
 import { IconButton, List, ListItem } from "@mui/material";
 import { Icon } from "@iconify/react";
 import { themeContext } from "../../homepage/home";
 import { newShade } from "../../App";
 import soft_click from "../../assets/sounds/soft_click.wav";
-import styles from "./todos.module.css"
+import styles from "./todos.module.css";
 
 const butonClick = new Audio(soft_click);
 butonClick.volume = 0.1;
 
-
 function Todos() {
-  const contextData = useContext(themeContext);
-  const theme = contextData.themes;
-  const user = contextData.user;
+    const contextData = useContext(themeContext);
+    const theme = contextData.themes;
+    const user = contextData.user;
 
-  const tabButtonStyle = {
-    backgroundColor: "transparent",
-    flex: 0.5,
-    fontWeight: "500",
-    fontSize: "15px",
-    marginTop: "2px",
-    borderRadius: "5px",
-    border: "1px solid #000000",
-    marginLeft: "px",
-    marginRight: "3px",
-    padding: "5px 5px",
-  };
+    const tabButtonStyle = {
+        backgroundColor: "transparent",
+        flex: 0.5,
+        fontWeight: "500",
+        fontSize: "15px",
+        marginTop: "2px",
+        borderRadius: "5px",
+        border: "1px solid #000000",
+        marginLeft: "px",
+        marginRight: "3px",
+        padding: "5px 5px",
+    };
 
-  const [allTodo, setAllTodo] = useState([]);
-  const todoRef = collection(db, "todos");
-  const q = query(todoRef, where("user", "==", user?.username));
+    const [allTodo, setAllTodo] = useState([]);
+    const todoRef = collection(db, "todos");
+    const q = query(todoRef, where("user", "==", user?.username));
 
-  const getCompleted = () => {
-    const completed = allTodo.filter((item) => item.completed === true);
-    return completed;
-  };
-  const getIncomplete = () => {
-    const incomplete = allTodo.filter((item) => item.completed === false);
-    return incomplete;
-  };
-  const [todo, setTodo] = useState([]);
-  // 1: all, 2: completed, 3: incomplete
-  const [tabSelected, setTabSelected] = useState(3);
+    const getCompleted = () => {
+        const completed = allTodo.filter((item) => item.completed === true);
+        return completed;
+    };
+    const getIncomplete = () => {
+        const incomplete = allTodo.filter((item) => item.completed === false);
+        return incomplete;
+    };
+    const [todo, setTodo] = useState([]);
+    // 1: all, 2: completed, 3: incomplete
+    const [tabSelected, setTabSelected] = useState(3);
 
-  const getAllTodos = async () => {
-    try {
-      const data = await getDocs(q);
-      const filteredData = data.docs.map((doc) => {
-        return { ...doc.data(), id: doc.id };
-      });
-      console.log(filteredData);
-      if (allTodo.length === 0)
-        setTodo(filteredData.filter((item) => item.completed === false));
-      setAllTodo(filteredData);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+    const getAllTodos = async () => {
+        try {
+            const data = await getDocs(q);
+            const filteredData = data.docs.map((doc) => {
+                return { ...doc.data(), id: doc.id };
+            });
+            console.log(filteredData);
+            if (allTodo.length === 0)
+                setTodo(
+                    filteredData.filter((item) => item.completed === false)
+                );
+            setAllTodo(filteredData);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-  useEffect(() => {
-    getAllTodos();
-  }, []);
+    useEffect(() => {
+        getAllTodos();
+    }, []);
 
-  useEffect(() => {
-    if (tabSelected === 1) setTodo(allTodo);
-    else if (tabSelected === 2) setTodo(getCompleted());
-    else setTodo(getIncomplete());
-  }, [allTodo]);
+    useEffect(() => {
+        if (tabSelected === 1) setTodo(allTodo);
+        else if (tabSelected === 2) setTodo(getCompleted());
+        else setTodo(getIncomplete());
+    }, [allTodo]);
 
-  const [todoItem, setTodoItem] = useState("");
-  const addItem = async (e) => {
-    await addDoc(todoRef, {
-      task: todoItem,
-      user: user?.username,
-      completed: false,
-    });
-    getAllTodos();
-    setTodoItem("");
-  };
+    const [todoItem, setTodoItem] = useState("");
+    const addItem = async (e) => {
+        await addDoc(todoRef, {
+            task: todoItem,
+            user: user?.username,
+            completed: false,
+        });
+        getAllTodos();
+        setTodoItem("");
+    };
 
-  const deleteItem = async (id) => {
-    const itemDoc = doc(db, "todos", id);
-    await deleteDoc(itemDoc);
-    getAllTodos();
-  };
+    const deleteItem = async (id) => {
+        const itemDoc = doc(db, "todos", id);
+        await deleteDoc(itemDoc);
+        getAllTodos();
+    };
 
-  const changeStatus = async (id) => {
-    const itemDoc = doc(db, "todos", id);
-    await updateDoc(itemDoc, {
-      completed: true,
-    });
-    getAllTodos();
-  };
+    const changeStatus = async (id) => {
+        const itemDoc = doc(db, "todos", id);
+        await updateDoc(itemDoc, {
+            completed: true,
+        });
+        getAllTodos();
+    };
 
-  const todoList = todo.map((item) => {
-    return (
-      <ListItem
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          width: "100%",
-          marginLeft: "5px",
-          padding: 0,
-          paddingLeft: "10px",
-          paddingRight: "10px",
-          // alignItems: "center",
-          // height: "50px",
-          "&:hover": {
-            backgroundColor: newShade(theme, -30),
-          }
-        }}
-        key={item.id}
-      >
-        <p style={{flex:0.9,fontSize:"19px"}}>
-          {item.task}{" "}
-        </p>
-        <div>
-          {!item.completed && (
-            <IconButton
-              title="Task Completed"
-              onClick={() => changeStatus(item.id)}
+    const todoList = todo.map((item) => {
+        return (
+            <ListItem
+                sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    width: "100%",
+                    marginLeft: "5px",
+                    padding: 0,
+                    paddingLeft: "10px",
+                    paddingRight: "10px",
+                    // alignItems: "center",
+                    // height: "50px",
+                    "&:hover": {
+                        backgroundColor: newShade(theme, -30),
+                    },
+                }}
+                key={item.id}
             >
-              <Icon icon="material-symbols:check" />
+                <p style={{ flex: 0.9, fontSize: "19px" }}>{item.task} </p>
+                <div>
+                    {!item.completed && (
+                        <IconButton
+                            title="Task Completed"
+                            onClick={() => changeStatus(item.id)}
+                        >
+                            <Icon icon="material-symbols:check" />
+                        </IconButton>
+                    )}
+                    <IconButton
+                        title="Delete Task"
+                        onClick={() => deleteItem(item.id)}
+                    >
+                        <Icon icon="iconoir:cancel" />
+                    </IconButton>
+                </div>
+            </ListItem>
+        );
+    });
+
+    const changeTab = (e) => {
+        butonClick.play();
+        setTabSelected(e.target.id);
+        if (e.target.id == 1) {
+            setTodo(allTodo);
+        } else if (e.target.id == 2) {
+            setTodo(getCompleted());
+        } else if (e.target.id == 3) {
+            setTodo(getIncomplete());
+        }
+    };
+
+    return (
+        <div className="todo_container">
+            <h2>My tasks</h2>
+            {/* Complete and incomplete tabs */}
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    marginTop: "10px",
+                }}
+            >
+                <button
+                    id="3"
+                    onClick={changeTab}
+                    style={{
+                        ...tabButtonStyle,
+                        backgroundColor:
+                            tabSelected == 3
+                                ? newShade(theme, -30)
+                                : "transparent",
+                    }}
+                >
+                    Active
+                </button>
+                <button
+                    id="2"
+                    onClick={changeTab}
+                    style={{
+                        ...tabButtonStyle,
+                        backgroundColor:
+                            tabSelected == 2
+                                ? newShade(theme, -30)
+                                : "transparent",
+                    }}
+                >
+                    Completed
+                </button>
+                <button
+                    id="1"
+                    onClick={changeTab}
+                    style={{
+                        ...tabButtonStyle,
+                        backgroundColor:
+                            tabSelected == 1
+                                ? newShade(theme, -30)
+                                : "transparent",
+                    }}
+                >
+                    All Tasks
+                </button>
+            </div>
+            <input
+                placeholder="Add an item"
+                id="todoInput"
+                onChange={(e) => setTodoItem(e.target.value)}
+                value={todoItem}
+            ></input>
+            <IconButton title="Add Item" onClick={addItem}>
+                <Icon icon="material-symbols:add" />
             </IconButton>
-          )}
-          <IconButton title="Delete Task" onClick={() => deleteItem(item.id)} >
-            <Icon icon="iconoir:cancel" />
-          </IconButton>
+            <div className="todo-list">
+                <List>{todoList}</List>
+            </div>
         </div>
-      </ListItem>
     );
-  });
-
-  const changeTab = (e) => {
-    butonClick.play();
-    setTabSelected(e.target.id);
-    if (e.target.id == 1) {
-      setTodo(allTodo);
-    } else if (e.target.id == 2) {
-      setTodo(getCompleted());
-    } else if (e.target.id == 3) {
-      setTodo(getIncomplete());
-    }
-  };
-
-  return (
-    <div className="todo_container">
-      <h2 >
-        My tasks
-      </h2>
-      {/* Complete and incomplete tabs */}
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          marginTop: "10px",
-        }}
-      >
-        <button
-          id="3"
-          onClick={changeTab}
-          style={{
-            ...tabButtonStyle,
-            backgroundColor:
-              tabSelected == 3 ? newShade(theme, -30) : "transparent",
-          }}
-        >
-          Active
-        </button>
-        <button
-          id="2"
-          onClick={changeTab}
-          style={{
-            ...tabButtonStyle,
-            backgroundColor:
-              tabSelected == 2 ? newShade(theme, -30) : "transparent",
-          }}
-        >
-          Completed
-        </button>
-        <button
-          id="1"
-          onClick={changeTab}
-          style={{
-            ...tabButtonStyle,
-            backgroundColor:
-              tabSelected == 1 ? newShade(theme, -30) : "transparent",
-          }}
-        >
-          All Tasks
-        </button>
-      </div>
-      <input
-        placeholder="Add an item"
-        id="todoInput"
-        onChange={(e) => setTodoItem(e.target.value)}
-        value={todoItem}
-      ></input>
-      <IconButton title="Add Item" onClick={addItem}>
-        <Icon icon="material-symbols:add" />
-      </IconButton>
-      <div className="todo-list">
-        <List>{todoList}</List>
-      </div>
-    </div>
-  );
 }
 
 export default Todos;
--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
-import {getAuth} from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "zen-backend.firebaseapp.com",
-  projectId: "zen-backend",
-  storageBucket: "zen-backend.appspot.com",
-  messagingSenderId: "816294930039",
-  appId: "1:816294930039:web:9e63e859909f98532b0872",
-  measurementId: "G-V8CSNWT2QM"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "yinlofi.firebaseapp.com",
+    databaseURL: "https://yinlofi-default-rtdb.firebaseio.com",
+    projectId: "yinlofi",
+    storageBucket: "yinlofi.appspot.com",
+    messagingSenderId: "484277865772",
+    appId: "1:484277865772:web:cc3b69dcbd87b5b7f7b089",
+    measurementId: "G-RG37BFM6ML",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
 
-export const db= getFirestore(app)
+export const db = getFirestore(app);
